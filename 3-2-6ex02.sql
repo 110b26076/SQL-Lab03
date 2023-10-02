d996793ddbef9127fc9fc2c12d32149d1f0fc2be
110b26076 <115057357+110b26076@users.noreply.github.com>
2023-10-02 17:11:42 +0800

    Add files via upload

---
+++ 3-2-6ex02.sql
@@ -0,0 +1,11 @@
+Insert Into Comp_dep
+Values('D001','生產部'),
+		('D002','銷售部')
+Insert Into Employee
+Values('S001','一心','D001'),
+		('S002','二聖','D002'),
+		('S003','三多','D002'),
+		('S004','四維','D002'),
+		('S005','五福','D002')
+Insert Into Employee
+Values('D003','研發部')
